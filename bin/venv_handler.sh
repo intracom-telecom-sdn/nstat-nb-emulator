94f8af89553004ae088e0d42927899f31dafeded
Konstantinos Papadopoulos <konpap@intracom-telecom.com>
2016-12-01 15:41:47 +0200

    Corrected venv_handler (in similar fashion with multinet/oftraf/...)

--- bin/venv_handler.sh
+++ bin/venv_handler.sh
@@ -9,13 +9,23 @@
 # This script is responsible for activating virtual environment for multinet
 # handlers.
 # Arguments:
-# 1. PYTHONPATH
-# 2. Handler path
+# 01. PYTHONPATH
+# 02. run_handler.py path
+# 03. --controller-ip
+# 04. --controller-port
+# 05. --number-of-flows
+# 06. --number-of-workers
+# 07. --operation-delay
+# 08. --delete-flows
+# 09. --restconf-user
+# 10. --restconf-password
+# 11. --fpr
+# 11. --logging-level
 
 if [ "$#" -eq 2 ]
 then
     echo 'Konstantinos Papadopoulos'
-    source /opt/venv_nb_generator/bin/activate; PYTHONPATH=$1 python3.4 $2
+    source /opt/venv_nb_generator/bin/activate; PYTHONPATH=$1; python3.4 $2 $3 $4 $5 $6 $7 $8 $9 $10 #11
 else
     echo "Invalid number of arguments."
     exit 1
