5f6c5f70621b7b57d75318b77de221a36eccccfe
thomas sounapoglou <soth@intracom-telecom.com>
2016-12-07 10:45:23 +0200

    Add fixed path for nb-gen

--- bin/venv_handler.sh
+++ bin/venv_handler.sh
@@ -28,8 +28,8 @@ if [ "$HANDLER_PATH" == "/opt/nstat/emulators/nb_generator/run_handler.py" ]; th
 # 12. --logging-level
 # ------------------------------------------------------------------------------
   if [ "$#" -eq 12 ]; then
-  	  echo $@
-      source /opt/venv_nb_generator/bin/activate; PYTHONPATH=$1; python3.4 $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12}
+#     source /opt/venv_nb_generator/bin/activate; PYTHONPATH=$1; python3.4 $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12}
+      source /opt/venv_nb_generator/bin/activate; PYTHONPATH=$1; python3.4 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12}
       exit 0
   else
       echo "Invalid number of arguments."
